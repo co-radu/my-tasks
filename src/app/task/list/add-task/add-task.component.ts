c0136b569c14f3b7cf5258407cf3212dd7cdafbc
Corentin Radureau <radureau.c@gmail.com>
2020-05-01 14:29:14 +0200

    Add add-task form in the modal.

--- src/app/task/list/add-task/add-task.component.ts
+++ src/app/task/list/add-task/add-task.component.ts
@@ -1,13 +1,17 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TaskService } from '../../task.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Task } from 'src/app/shared/models/task.model';
 
 @Component({
     selector: 'add-task',
     templateUrl: './add-task.component.html'
 })
 
-export class AddTaskComponent {
+export class AddTaskComponent implements OnInit {
+
+    public form : FormGroup;
 
     constructor(
         private taskService: TaskService,
@@ -15,4 +19,35 @@ export class AddTaskComponent {
         @Inject(MAT_DIALOG_DATA) public data: any,
     ) {}
 
+    ngOnInit(): void {
+        this.form = new FormGroup({
+            label: new FormControl('', Validators.required),
+            description: new FormControl(''),
+            position: new FormControl(0),
+            createdDate: new FormControl(new Date),
+            isActive: new FormControl(true),
+        })
+    }
+
+    addTask(): void {
+        if (this.form.invalid)
+            return;
+        const task: Task = {
+            label: this.form.get('label').value,
+            description: this.form.get('description').value,
+            position: this.form.get('position').value,
+            createdDate: this.form.get('createdDate').value,
+            isActive: this.form.get('isActive').value,
+        };
+        this.taskService.addTask(task).subscribe(
+        );
+        this.onNoClick();
+        error => {
+            console.log('ERREUR' + error);
+        };
+    }
+
+    onNoClick(): void {
+        this.dialogRef.close();
+      }
 }
--- src/app/task/list/task-list.component.ts
+++ src/app/task/list/task-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TaskService } from '../task.service';
 import { Task } from 'src/app/shared/models/task.model';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { moveItemInArray, CdkDragDrop } from '@angular/cdk/drag-drop';
@@ -16,7 +15,6 @@ import { moveItemInArray, CdkDragDrop } from '@angular/cdk/drag-drop';
 export class TaskListComponent implements OnInit {
 
     public tasks: Task[] = [];
-    public form: FormGroup;
 
     constructor(
         private router: Router,
@@ -25,18 +23,9 @@ export class TaskListComponent implements OnInit {
     ) { }
 
     ngOnInit(): void {
-        this.form = new FormGroup({
-            label: new FormControl('', Validators.required),
-            description: new FormControl(''),
-            position: new FormControl(0),
-            createdDate: new FormControl(new Date),
-            isActive: new FormControl(true),
-        });
-
         this.taskService.getTasks().subscribe(
             (tasks: Task[]) => {
                 this.tasks = tasks;
-                /*this.deleteTask(this.tasks[0]);*/
             }
         );
     }
@@ -63,23 +52,6 @@ export class TaskListComponent implements OnInit {
                 console.log('ERREUR' + error);
             }
         );
-    }
-
-    addTask(): void {
-        if (this.form.invalid)
-            return;
-        const task: Task = {
-            label: this.form.get('label').value,
-            description: this.form.get('description').value,
-            position: this.form.get('position').value,
-            createdDate: this.form.get('createdDate').value,
-            isActive: this.form.get('isActive').value,
-        };
-        this.taskService.addTask(task).subscribe(
-        );
-        error => {
-            console.log('ERREUR' + error);
-        };
     }
 
     drop(event: CdkDragDrop<string[]>) {
