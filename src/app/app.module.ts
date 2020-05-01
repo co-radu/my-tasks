2ac58cb2235a854ec71e660b34ceab1d7e568185
Corentin Radureau <radureau.c@gmail.com>
2020-05-01 11:02:57 +0200

    Init modal add-task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
 import { MaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { TaskService } from './task/task.service';
 import { TaskListComponent } from './task/list/task-list.component';
 import { DetailTaskComponent } from './task/detail/detail-task.component';
-import { AddTaskComponent } from './task/add-task/add-task.component';
+import { AddTaskComponent } from './task/list/add-task/add-task.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
---
+++ src/app/task/list/add-task/add-task.component.ts
@@ -0,0 +1,17 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { TaskService } from '../../task.service';
+
+@Component({
+    selector: 'add-task',
+    templateUrl: './add-task.component.html'
+})
+
+export class AddTaskComponent {
+
+    constructor(
+        private taskService: TaskService,
+        public dialogRef: MatDialogRef<AddTaskComponent>,
+    ) {}
+
+}
